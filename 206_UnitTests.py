ec4472bc8e65f792f2039a8015c65d021b2850f2
vinhluong274 <vinhluong274@gmail.com>
2017-10-02 23:49:59 -0400

    Finished Homework 4

---
+++ 206_UnitTests.py
@@ -0,0 +1,217 @@
+import random
+import unittest
+
+# Vinh Luong
+# vinhbl@umich.edu
+
+# SI 206 Fall 2017
+# Homework 3 - Code
+
+##COMMENT YOUR CODE WITH:
+# Section THURSDAY, 6PM
+# People you worked with: Joshua Walker
+
+######### DO NOT CHANGE PROVIDED CODE #########
+### Below is the same cards.py code you saw in lecture.
+### Scroll down for assignment instructions.
+#########
+
+class Card(object):
+	suit_names =  ["Diamonds","Clubs","Hearts","Spades"]
+	rank_levels = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+	faces = {1:"Ace",11:"Jack",12:"Queen",13:"King"}
+
+	def __init__(self, suit=0,rank=2):
+		self.suit = self.suit_names[suit]
+		if rank in self.faces: # self.rank handles printed representation
+			self.rank = self.faces[rank]
+		else:
+			self.rank = rank
+		self.rank_num = rank # To handle winning comparison
+
+	def __str__(self):
+		return "{} of {}".format(self.rank,self.suit)
+
+class Deck(object):
+	def __init__(self): # Don't need any input to create a deck of cards
+		# This working depends on Card class existing above
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1,14):
+				card = Card(suit,rank)
+				self.cards.append(card) # appends in a sorted order
+
+	def __str__(self):
+		total = []
+		for card in self.cards:
+			total.append(card.__str__())
+		# shows up in whatever order the cards are in
+		return "\n".join(total) # returns a multi-line string listing each card
+
+	def pop_card(self, i=-1):
+		# removes and returns a card from the Deck
+		# default is the last card in the Deck
+		return self.cards.pop(i) # this card is no longer in the deck -- taken off
+
+	def shuffle(self):
+		random.shuffle(self.cards)
+
+	def replace_card(self, card):
+		card_strs = [] # forming an empty list
+		for c in self.cards: # each card in self.cards (the initial list)
+			card_strs.append(c.__str__()) # appends the string that represents that card to the empty list
+		if card.__str__() not in card_strs: # if the string representing this card is not in the list already
+			self.cards.append(card) # append it to the list
+
+	def sort_cards(self):
+		# Basically, remake the deck in a sorted way
+		# This is assuming you cannot have more than the normal 52 cars in a deck
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1,14):
+				card = Card(suit,rank)
+				self.cards.append(card)
+
+
+def play_war_game(testing=False):
+	# Call this with testing = True and it won't print out all the game stuff -- makes it hard to see test results
+	player1 = Deck()
+	player2 = Deck()
+
+	p1_score = 0
+	p2_score = 0
+
+	player1.shuffle()
+	player2.shuffle()
+	if not testing:
+		print("\n*** BEGIN THE GAME ***\n")
+	for i in range(52):
+		p1_card = player1.pop_card()
+		p2_card = player2.pop_card()
+		if not testing:
+			print("Player 1 plays", p1_card,"& Player 2 plays", p2_card)
+
+		if p1_card.rank_num > p2_card.rank_num:
+			if not testing:
+				print("Player 1 wins a point!")
+			p1_score += 1
+		elif p1_card.rank_num < p2_card.rank_num:
+			if not testing:
+				print("Player 2 wins a point!")
+			p2_score += 1
+		else:
+			if not testing:
+				print("Tie. Next turn.")
+
+	if p1_score > p2_score:
+		return "Player1", p1_score, p2_score
+	elif p2_score > p1_score:
+		return "Player2", p1_score, p2_score
+	else:
+		return "Tie", p1_score, p2_score
+
+if __name__ == "__main__":
+	result = play_war_game()
+	print("""\n\n******\nTOTAL SCORES:\nPlayer 1: {}\nPlayer 2: {}\n\n""".format(result[1],result[2]))
+	if result[0] != "Tie":
+		print(result[0], "wins")
+	else:
+		print("TIE!")
+
+
+######### DO NOT CHANGE CODE ABOVE THIS LINE #########
+
+## You can write any additional debugging/trying stuff out code here...
+## OK to add debugging print statements, but do NOT change functionality of existing code.
+## Also OK to add comments!
+
+#########
+
+
+
+
+
+##**##**##**##@##**##**##**## # DO NOT CHANGE OR DELETE THIS COMMENT LINE -- we use it for grading your file
+###############################################
+
+### Write unit tests below this line for the cards code above.
+class CardTests(unittest.TestCase):
+	# Test to see if a card is rank 12, it will be a Queen
+	def test_cardrank(self):
+		c = Card(rank=12)
+		self.assertEqual(c.rank, "Queen")
+
+	# Test to see if a card is rank 1, it will be an Ace
+	def test_cardrank1(self):
+		c = Card(rank=1)
+		self.assertEqual(c.rank, "Ace")
+
+	# Test that if you create a card instance with rank 3, it will be a 3
+	def test_cardrank2(self):
+		c = Card(rank=3)
+		self.assertEqual(c.rank, 3)
+
+	# test suit 1 will be "Clubs"
+	def test_cardsuit(self):
+		c = Card(suit=1)
+		self.assertEqual(c.suit, "Clubs")
+
+	# test suit 2 will be "Hearts"
+	def test_cardsuit2(self):
+		c = Card(suit=2)
+		self.assertEqual(c.suit, "Hearts")
+
+# Test that if you create a card instance, it will have access to a variable
+# suit_names that contains the list ["Diamonds","Clubs","Hearts","Spades"]
+	def test_Instance(self):
+		c = Card()
+		self.assertEqual(c.suit_names, ["Diamonds","Clubs","Hearts","Spades"])
+
+## Test that if you invoke the __str__ method of a card instance that
+# is created with suit=2, rank=7, it returns the string "7 of Hearts"
+	def test_cardInstance2(self):
+		c = Card(suit=2, rank = 7)
+		self.assertEqual(str(c),"7 of Hearts")
+
+## Test that if you create a deck instance, it will have 52 cards in
+# its cards instance variable
+	def test_deckInstance(self):
+		d = Deck()
+		self.assertEqual(len(d.cards),52)
+
+## Test that if you invoke the pop_card method on a deck, it will
+# return a card instance.
+	def test_popInstance(self):
+		d = Deck()
+		c = Card()
+		self.assertEqual(type(d.pop_card()),type(c))
+
+	## Test that the return value of the play_war_game function is a tuple
+    # with three elements, the first of which is a string. (This will
+    # probably require multiple test methods!)
+	def test_warInstance(self):
+		p = play_war_game(testing="True")
+		self.assertEqual(len(p),3)
+		self.assertEqual(type(p[0]), str)
+
+
+# test that if a card is created, it will be able to access variable "faces"
+# that contains a dictionary with keys 1, 2, 3, 4, and values Ace, Jack, Queen,
+# and King, respectively.
+	def test_mytestInstance1(self):
+		c = Card()
+		self.assertEqual(c.faces,{1:"Ace",11:"Jack",12:"Queen",13:"King"})
+
+# Test to see that if the deck is sorted, the card you pop from the deck will be the highest rank
+# invoke the str method to show that it will be the King of Spades.
+	def test_mytestInstance2(self):
+		d = Deck()
+		d.sort_cards()
+		self.assertEqual(str(d.pop_card()), "King of Spades")
+
+
+
+#############
+## The following is a line to run all of the tests you include:
+unittest.main(verbosity=2)
+## verbosity 2 to see detail about the tests the code fails/passes/etc.
